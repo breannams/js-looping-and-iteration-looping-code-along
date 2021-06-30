2b9c7688899ca91671b99f693e139278b4243785
breannams <breannamarie_s@hotmail.com>
2021-06-30 19:49:10 -0400

    commit tree 07971dfe60ba51163d6da74097af6ae806dc598f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, event){
+    let cardMsg = [];
+for (let i = 0; i < namesArray.length; i++)
+ cardMsg.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    return cardMsg
+}
+
+function countDown(i){
+    while (i >= 0){
+        console.log(i--)
+    }
+}
